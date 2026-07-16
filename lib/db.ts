dc2eaf1ffa36850c48bf95a1d7f5eefad0da445e
fathan fadly hidayat <fatannnfdly@gmail.com>
2026-07-16 23:08:25 +0700

    feat: support DATABASE_URL in db config

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,19 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'radiocityguide',
+const pool = mysql.createPool(
+    process.env.DATABASE_URL 
+    ? {
+        uri: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    } 
+    : {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'radiocityguide',
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
     waitForConnections: true,
     connectionLimit: 10,
